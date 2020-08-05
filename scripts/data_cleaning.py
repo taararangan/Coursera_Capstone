e78c702c50c010b3ebe309a16080b70190e8b32a
Taara <taara.rangan@gmail.com>
2020-08-05 13:57:27 -0700

    added initial capstone code

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,5 @@
+# this notebook will be used to conduct the "battle of the neighborhoods" final for the IBM Data Science Capstone
+import pandas as pd
+import numpy as np
+
+print "Hello Capstone Project Course!"
