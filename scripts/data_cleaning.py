091baa4abc27a05d133fbfb3222698209459a8b5
Taara <taara.rangan@gmail.com>
2020-08-06 10:20:47 -0700

    removing extraneous file

--- scripts/data_cleaning.py
+++
@@ -1,5 +0,0 @@
-# this notebook will be used to conduct the "battle of the neighborhoods" final for the IBM Data Science Capstone
-import pandas as pd
-import numpy as np
-
-print "Hello Capstone Project Course!"
